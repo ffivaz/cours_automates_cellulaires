915d25776ac0e6a80ec91ad1f7ee9f57fd3c547e
Fabien Fivaz <fabien.fivaz@unine.ch>
2016-08-31 10:08:09 +0200

    Création système pour jeu de la vie

---
+++ game_of_life/js/script.js
@@ -0,0 +1,9 @@
+var nRows = 8;
+var nCols = 24;
+
+var nPixels = nCols * nRows;
+var pixelSize = 20;
+
+$(".canvas").css("width", nCols * pixelSize + "px");
+$('.canvas').css("height", nRows * pixelSize + "px");
+
