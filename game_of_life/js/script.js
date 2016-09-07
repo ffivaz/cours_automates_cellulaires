2d0c36710fb6ce60085a014befd58ad747eed7f3
ffivaz <fabien.fivaz@unine.ch>
2016-09-07 15:39:27 +0200

    Major change!

--- game_of_life/js/script.js
+++
@@ -1,9 +0,0 @@
-var nRows = 8;
-var nCols = 24;
-
-var nPixels = nCols * nRows;
-var pixelSize = 20;
-
-$(".canvas").css("width", nCols * pixelSize + "px");
-$('.canvas').css("height", nRows * pixelSize + "px");
-
