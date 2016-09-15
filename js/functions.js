17be7ce4fa3c2f3449b3c50ac17556d4183b9d77
csfivaz <_Beck1640>
2016-09-15 15:16:49 +0200

    Game of Life created

--- js/functions.js
+++ js/functions.js
@@ -27,3 +27,21 @@ Array.prototype.equals = function (array) {
 }
 // Hide method from for-in loops
 Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+function arrayClone( arr ) {
+
+    var i, copy;
+
+    if( Array.isArray( arr ) ) {
+        copy = arr.slice( 0 );
+        for( i = 0; i < copy.length; i++ ) {
+            copy[ i ] = arrayClone( copy[ i ] );
+        }
+        return copy;
+    } else if( typeof arr === 'object' ) {
+        throw 'Cannot clone array containing an object!';
+    } else {
+        return arr;
+    }
+
+}
